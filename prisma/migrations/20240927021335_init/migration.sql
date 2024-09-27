15535af8bcf7ffd123993526224e94835c2affb5
acelee0621 <acelee0621@outlook.com>
2024-09-27 17:19:55 +0800

    更换数据库

--- prisma/migrations/20240927021335_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "userId" INTEGER NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_userId_key" ON "User"("userId");
